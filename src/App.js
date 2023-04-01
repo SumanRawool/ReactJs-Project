30317239e96832755d2004a7f4a63a808433881d
Suman <sumanrawool2@gmail.com>
2023-04-01 11:47:38 +0530

    learn useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,60 +1,36 @@
 import "./App.css";
 import Header from "./components/Header";
-import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import AddIcon from "@mui/icons-material/Add";
-import Fields from "./components/Fields";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [data, setData] = useState([]);
+  const [state, setState] = useState(2);
+  const [data, setData] = useState();
 
-  const addData = () => {
-    setData([...data, { name, email }]);
-    setName("");
-    setEmail("");
-  };
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+      );
+      const res = await get.json();
+      setData(res);
+    }
+    getData();
+    document.title = `(${state}) Employees Online`;
+  }, [state]);
+
   return (
-    <div className="App">
+    <div>
       <Header />
-      {/* form */}
-      <div className="form">
-        <Stack direction="row" spacing={2}>
-          <TextField
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-          />
-          <TextField
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-          />
-          <Button onClick={addData} variant="contained" color="error">
-            <AddIcon />
-          </Button>
-        </Stack>
-      </div>
-      {/* Data */}
-      <div className="data">
-        <div className="data_val">
-          <h4>Name</h4>
-          <h4>Email</h4>
-          <h4>Remove</h4>
-        </div>
-        {data.map((element, index) => {
-          return (
-            <Fields name={element.name} email={element.email} index={index} />
-          );
-        })}
-      </div>
+      <button onClick={() => setState(state + 2)}>Click Me {state}</button>
+      {data?.map((element, index) => {
+        return (
+          <div className="data" key={index}>
+            <h1>{element.firstName}</h1>
+            <h1>{element.lastName}</h1>
+            <h1>{element.email}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
